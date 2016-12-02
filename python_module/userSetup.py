cd89daaffd4642659d8e3a1655747627e6b1fa96
yamahigashi <yamahigashi+github@gmail.com>
2016-12-02 12:46:20 +0900

    fix registering menu

--- python_module/userSetup.py
+++ python_module/userSetup.py
@@ -11,14 +11,27 @@ import mayaenveditor as editor
 
 def create_menu():
     cmd = '''
-    global string $gMainFileMenu; // Retrieve the menu name
-    buildViewMenu("MayaWindow|mainWindowMenu");
+    buildViewMenu MayaWindow|mainWindowMenu;
     buildPreferenceMenu mainOptionsMenu;
-    buildSettingsMenu MayaWindow|mainWindowMenu;
 
+    string $parentName = "MayaWindow|mainWindowMenu";
+    string $menuItems[] = `menu -q -ia $parentName`;
+
+    for ($i = 0; $i < size($menuItems); $i += 1) {
+        string $label = `menuItem -q -label $menuItems[$i]`;
+        string $match = `match "Settings" $label`;
+        if (0 < size($match)){
+            $parentName = $parentName + "|" + $menuItems[$i];
+            break;
+        }
+    }
+
+    buildSettingsMenu $parentName;
+    setParent -menu $parentName;
     '''
 
     mel.eval(cmd)
+
     cmds.menuItem(divider=True)
     cmds.menuItem(label="Edit Maya.env", command=partial(editor.run))
 
@@ -28,7 +41,7 @@ def later():
     Creates mGear menu
     """
     try:
-        print "create_menu"
+        print "create_menu for mayaenveditor"
         create_menu()
         print "finish create_menu"
 
